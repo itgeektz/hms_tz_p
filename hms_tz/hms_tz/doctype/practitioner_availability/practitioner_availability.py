076ccebd071f8e372965cc5e2f66dfb2685228a4
Info Aakvatech <info@aakvatech.com>
2020-12-26 20:41:34 +0000

    fix: Practitioner Availability for type present not allowed to save as the message popup is creating error. missign import

--- hms_tz/hms_tz/doctype/practitioner_availability/practitioner_availability.py
+++ hms_tz/hms_tz/doctype/practitioner_availability/practitioner_availability.py
@@ -4,7 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
-import frappe
+from frappe import _
 from frappe.model.document import Document
 from frappe.utils import time_diff_in_seconds, getdate, formatdate
 
