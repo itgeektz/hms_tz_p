409ff449a0c6fa5f69d29474e7a837705792eb81
Info Aakvatech <info@aakvatech.com>
2020-12-30 05:36:12 +0000

    fix: Non NHIF insurance geting patient info

--- hms_tz/nhif/api/insurance_subscription.js
+++ hms_tz/nhif/api/insurance_subscription.js
@@ -7,8 +7,19 @@ frappe.ui.form.on('Healthcare Insurance Subscription', {
 		});
     },
     coverage_plan_card_number: function(frm){
-        if (!frm.doc.coverage_plan_card_number) return
-        frappe.call({
+        frm.trigger("get_patient_name")
+    },
+    insurance_company: function(frm){
+        frm.trigger("get_patient_name")
+        frm.set_value("daily_limit", 0)
+    },
+    get_patient_name: function(frm){
+        if (!frm.doc.coverage_plan_card_number || frm.doc.insurance_company != "NHIF") return
+        frappe.show_alert({
+            message:__("Getting patient's information from NHIF"),
+            indicator:'green'
+        }, 5);
+            frappe.call({
             method: 'hms_tz.nhif.api.insurance_subscription.check_patient_info',
             args: {
                 'card_no': frm.doc.coverage_plan_card_number,
