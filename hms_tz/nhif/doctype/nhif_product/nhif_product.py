4fc14ff7354751a55e2d61289c6b25dee62b7a4a
Vimal <info@aakvatech.com>
2021-01-04 16:11:24 +0000

    fix: If NHIF returns with product ID blank it should skip creating NHIF product record. RIght now it fails

--- hms_tz/nhif/doctype/nhif_product/nhif_product.py
+++ hms_tz/nhif/doctype/nhif_product/nhif_product.py
@@ -6,23 +6,26 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class NHIFProduct(Document):
-	pass
+    pass
 
 
 def add_product(id, name=None):
-	if frappe.db.exists('NHIF Product', id):
-		if name and name != "null":
-			doc = frappe.get_doc('NHIF Product', id)
-			if not doc.product_name:
-				doc.product_name = name
-				doc.save(ignore_permissions=True)
-				frappe.db.commit()
-	else:
-		doc = frappe.new_doc('NHIF Product')
-		doc.product_id = id
-		if name and name != "null":
-			doc.product_name = name
-		doc.save(ignore_permissions=True)
-		frappe.db.commit()
-	return id
+    if not id:
+        return
+    if frappe.db.exists('NHIF Product', id):
+        if name and name != "null":
+            doc = frappe.get_doc('NHIF Product', id)
+            if not doc.product_name:
+                doc.product_name = name
+                doc.save(ignore_permissions=True)
+                frappe.db.commit()
+    else:
+        doc = frappe.new_doc('NHIF Product')
+        doc.product_id = id
+        if name and name != "null":
+            doc.product_name = name
+        doc.save(ignore_permissions=True)
+        frappe.db.commit()
+    return id
