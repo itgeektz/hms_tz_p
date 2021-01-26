9ed729bab537027020f57f2856665433dc24868a
Vimal <info@aakvatech.com>
2021-01-26 16:43:05 +0000

    feat: NHIF Physician Qualification doctype added for external referral

---
+++ hms_tz/nhif/doctype/nhif_physician_qualification/nhif_physician_qualification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NHIF Physician Qualification', {
+	// refresh: function(frm) {
+
+	// }
+});
