45c69b01a939eab6c2c34f9573ba300e883b3f20
Youssef Restom <youssefrestom@gmail.com>
2021-02-11 20:12:30 +0000

    feat: refactor Recently prescribed alert

--- hms_tz/nhif/api/delivery_note.py
+++ hms_tz/nhif/api/delivery_note.py
@@ -8,21 +8,26 @@ def validate(doc, method):
 
 
 def onload(doc, method):
-    set_prescribed(doc)
+    for item in doc.items:
+        if item.last_qty_prescribed:
+            frappe.msgprint(
+                _("The item {0} was last prescribed on {1} for {2} {3}").format(item.item_code, item.last_date_prescribed, item.last_qty_prescribed, item.stock_uom))
 
 
 def set_prescribed(doc):
     if doc.docstatus != 0:
         return
-    note_list_names = []
-    note_list = frappe.get_all("Delivery Note", filtes={
-        "docstatus": 1, "patient": doc.patient}, order_by='posting_date desc')
-    for note in note_list:
-        note_list_names.append(note.name)
+
     for item in doc.items:
-        items_list = frappe.get_all("Delivery Note Item", filtes={
-            "parent": ['in', note_list_names], "item_code": item.item_code}, fields=["qty", "uom", "parent"], order_by='modified desc')
+        items_list = frappe.db.sql("""
+        select dn.posting_date, dni.item_code, dni.stock_qty, dni.uom from `tabDelivery Note` dn
+        inner join `tabDelivery Note Item` dni on dni.parent = dn.name
+                        where dni.item_code = %s
+                        and dn.patient = %s
+                        and dn.docstatus = 1
+                        order by posting_date desc
+                        limit 1""" %
+                                   ('%s', '%s'), (item.item_code, doc.patient), as_dict=1)
         if len(items_list):
-            item.last_qty_prescribed = items_list[0].qty
-            item.last_date_prescribed = frappe.get_value(
-                "Delivery Note", items_list[0].parent, "posting_date")
+            item.last_qty_prescribed = items_list[0].get("stock_qty")
+            item.last_date_prescribed = items_list[0].get("posting_date")
