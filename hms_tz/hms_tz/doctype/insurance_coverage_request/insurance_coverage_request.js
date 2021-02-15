f2914c7652b76b84fae36c5dad5974cf40f1c821
Info Aakvatech <info@aakvatech.com>
2021-02-15 18:09:15 +0000

    fix: Missing insurance coverage request doctype complain due to hms ess merge

---
+++ hms_tz/hms_tz/doctype/insurance_coverage_request/insurance_coverage_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Insurance Coverage Request', {
+	// refresh: function(frm) {
+
+	// }
+});
