06111ed8817414071f58ffa89b0d45442df0cca8
Info Aakvatech <info@aakvatech.com>
2021-02-17 19:13:04 +0000

    feat: Encounter Category to allow filtering of Patient Encounter that can be viewed by receptionist

---
+++ hms_tz/hms_tz/doctype/encounter_category/encounter_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Encounter Category', {
+	// refresh: function(frm) {
+
+	// }
+});
