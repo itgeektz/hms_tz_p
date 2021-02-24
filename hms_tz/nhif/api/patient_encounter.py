b9b1c5eaad4fcf64f52fe624b9ac017acd3108a3
Info Aakvatech <info@aakvatech.com>
2021-02-24 09:17:07 +0000

    fix: Stop before setting prescribe to 1 if stock is not available

--- hms_tz/nhif/api/patient_encounter.py
+++ hms_tz/nhif/api/patient_encounter.py
@@ -25,11 +25,11 @@ def validate(doc, method):
     for key, value in child_tables.items():
         table = doc.get(key)
         for row in table:
-            if not doc.insurance_subscription:
-                row.prescribe = 1
             if not row.get("prescribe"):
                 validate_stock_item(row.get(value), row.get(
                     "quantity") or 1, healthcare_service_unit = row.get("healthcare_service_unit"))
+            if not doc.insurance_subscription:
+                row.prescribe = 1
 
     if not insurance_subscription:
         return
