259e035ca887f2cf44c0425ebe9bc96192cc4e33
Youssef Restom <youssefrestom@gmail.com>
2021-03-06 11:30:26 +0000

    feat: Prevent modification Inpatient Occupancy row after being invoiced

--- hms_tz/nhif/api/inpatient_record.js
+++ hms_tz/nhif/api/inpatient_record.js
@@ -3,6 +3,39 @@ frappe.ui.form.on('Inpatient Record', {
 });
 
 frappe.ui.form.on('Inpatient Occupancy', {
+    before_inpatient_occupancies_remove: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    before_inpatient_occupancies_remove_all: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    inpatient_occupancies_add: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    inpatient_occupancies_remove: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    inpatient_occupancies_move: (frm, cdt, cdn) => {
+        control_inpatient_record_move(frm, cdt, cdn);
+    },
+    form_render: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    check_in: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    check_out: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    invoiced: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    service_unit: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
+    left: (frm, cdt, cdn) => {
+        control_inpatient_record_permissing(frm, cdt, cdn);
+    },
     confirmed: (frm, cdt, cdn) => {
         let row = frappe.get_doc(cdt, cdn);
         if (row.invoiced || !row.left) return;
@@ -22,6 +55,20 @@ frappe.ui.form.on('Inpatient Occupancy', {
                 frm.reload_doc();
             }
         });
-
-    }
+    },
 });
+
+const control_inpatient_record_permissing = (frm, cdt, cdn) => {
+    let row = frappe.get_doc(cdt, cdn);
+    if (row.invoiced) {
+        frm.reload_doc();
+        frappe.throw(__(`This line has been invoiced. It cannot be modified or deleted`));
+    }
+};
+
+const control_inpatient_record_move = (frm, cdt, cdn) => {
+    let row = frappe.get_doc(cdt, cdn);
+    frm.reload_doc();
+    frappe.throw(__(`This line should not be moved`));
+
+};
