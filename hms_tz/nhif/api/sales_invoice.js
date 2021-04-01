0ee9e6294e4dc9a29b466b5b4aff5b6ae911ac2c
Info Aakvatech <info@aakvatech.com>
2021-04-01 06:08:46 +0000

    feat: Add buttons for receptionist to click directly

--- hms_tz/nhif/api/sales_invoice.js
+++ hms_tz/nhif/api/sales_invoice.js
@@ -13,7 +13,27 @@ frappe.ui.form.on('Sales Invoice', {
             frm.remove_custom_button('Healthcare Services', 'Get Items From');
             frm.remove_custom_button('Prescriptions', 'Get Items From');
 			if (cint(frm.doc.docstatus==0) && cur_frm.page.current_view_name!=="pos" && !frm.doc.is_return) {
-				frm.add_custom_button(__('Healthcare Services'), function() {
+				frm.add_custom_button(__('Get Healthcare Services'), function () {
+					get_healthcare_services_to_invoice(frm);
+				});
+				frm.add_custom_button(__('Get Sales Order'),
+					function () {
+						erpnext.utils.map_current_doc({
+							method: "erpnext.selling.doctype.sales_order.sales_order.make_sales_invoice",
+							source_doctype: "Sales Order",
+							target: me.frm,
+							setters: {
+								customer: me.frm.doc.customer || undefined,
+							},
+							get_query_filters: {
+								docstatus: 1,
+								status: ["not in", ["Closed", "On Hold"]],
+								per_billed: ["<", 99.99],
+								company: me.frm.doc.company
+							}
+						})
+					});
+				frm.add_custom_button(__('Healthcare Services'), function () {
 					get_healthcare_services_to_invoice(frm);
 				},"Get Items From");
 			}
