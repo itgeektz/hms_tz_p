85d5d527006024e2827a47c8f0172834f1ce90bf
Aakvatech Limited <info@aakvatech.com>
2021-11-18 21:00:37 +0100

    feat: Medication Category added

---
+++ hms_tz/hms_tz/doctype/medication_category/medication_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Medication Category', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hms_tz/hms_tz/doctype/medication_category/test_medication_category.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Aakvatech and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestMedicationCategory(unittest.TestCase):
+	pass
