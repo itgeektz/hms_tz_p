bc3b3dde2628f74b5abde9949d1dd190c3747f87
eliusmgani <eliasmgani@gmail.com>
2022-01-11 19:09:09 +0000

    fix: duplication of items due to duplication of check_in date

--- hms_tz/hms_tz/report/ipd_billing_report/ipd_billing_report.py
+++ hms_tz/hms_tz/report/ipd_billing_report/ipd_billing_report.py
@@ -35,7 +35,7 @@ def get_columns():
 def get_data(args):
 	service_list = []
 
-	service_units = get_inpatient_details(args)
+	service_units = get_inpatient_details(args["inpatient_record"])
 	
 	start_date = service_units[0]["check_in"].strftime("%Y-%m-%d")
 	end_date = service_units[-1]["check_in"].strftime("%Y-%m-%d")
@@ -46,16 +46,12 @@ def get_data(args):
 		total_amount = total_lab_amount = total_radiology_amount = 0
 		total_procedure_amount = total_drug_amount = total_therapy_amount = 0
 
-		checkin_date = service.check_in.strftime("%Y-%m-%d")
-
-		rate = flt(service.rate)
-		amount = flt(service.amount)
+		checkin_date = service["check_in"].strftime("%Y-%m-%d")
 
 		for encounter in encouter_transactions:
 			encounter_date = encounter["encounter_date"].strftime("%Y-%m-%d")
 
 			if checkin_date == encounter_date:
-
 				total_lab_amount += encounter["lab_amount"]
 				total_radiology_amount += encounter["radiology_amount"]
 				total_procedure_amount += encounter["procedure_amount"]
@@ -63,13 +59,13 @@ def get_data(args):
 				total_therapy_amount += encounter["therapy_amount"]
 				total_amount += encounter["total_amount"]
 		
-		grand_total = total_amount + amount + rate
+		grand_total = total_amount + service["inpatient_charges"]
 				
 		service_list.append({
 			"checkin_date": checkin_date,
 			"service_unit": service["service_unit"],
 			"service_unit_type": service["service_unit_type"],
-			"inpatient_charges": amount + rate,
+			"inpatient_charges": service["inpatient_charges"],
 			"total_lab_amount": total_lab_amount,
 			"total_radiology_amount": total_radiology_amount,
 			"total_procedure_amount": total_procedure_amount,
@@ -117,11 +113,11 @@ def get_encounter_data(args, start_date, end_date):
 			procedure_amount += procedure.amount
 
 		drug_transactions = frappe.get_all("Drug Prescription", 
-			filters={"prescribe": 1, "is_not_available_inhouse": 0, "is_cancelled": 0, "parent": enc.name},
-			fields=["quantity", "amount"]
+			filters={"prescribe": 1, "is_not_available_inhouse": 0, "parent": enc.name},
+			fields=["quantity", "quantity_returned", "amount"]
 		)
 		for drug in drug_transactions:
-			amount = drug.quantity * drug.amount
+			amount = (drug.quantity - drug.quantity_returned) * drug.amount
 			drug_amount += amount
 
 		therapy_transactions = frappe.get_all("Therapy Plan Detail", 
@@ -145,51 +141,107 @@ def get_encounter_data(args, start_date, end_date):
 
 	return encounter_services
 
-def get_inpatient_details(args):
+def get_inpatient_details(inpatient_record):
+	inpatient_list = []	
+
+	bed_details = get_occupancy_details(inpatient_record)
+	cons_details = get_consultancy_details(inpatient_record)
+
+	for bed in bed_details:
+		inpatient_charges1 = inpatient_charges2 = 0
+		for cons in cons_details:
+			if bed.check_in == cons.date:
+				inpatient_charges1 = bed.amount + cons.rate
+			
+			if bed.check_in != cons.date:
+				inpatient_charges2 = bed.amount or cons.rate
+		
+		inpatient_list.append({
+			"check_in": bed.check_in,
+			"service_unit": bed.service_unit,
+			"service_unit_type": bed.service_unit_type,
+			"inpatient_charges": inpatient_charges1 or inpatient_charges2
+		})
+	return inpatient_list
+
+def get_consultancy_details(inpatient_record):
+	date_list = []
+	cons_list = []
+	consultancy_list = []
+	duplicated_date_list = []
 	
-	parent = args['inpatient_record']
+	consultancies = frappe.get_all("Inpatient Consultancy", 
+		filters={"parent": inpatient_record,"is_confirmed": 1},
+        fields=["date", "rate"], order_by="date ASC"
+    )
+
+	for cons in consultancies:
+		if cons.date not in date_list:
+			date_list.append(cons.date)
+			cons_list.append(cons)
+		else:
+			duplicated_date_list.append(cons)
+	
+	for item in cons_list:
+		rate = 0
+		if duplicated_date_list:
+			for d in duplicated_date_list:
+				if item.date == d.date:
+					rate += d.rate
+		item.update({
+			"rate": flt(item.rate) + rate
+		})
+		consultancy_list.append(item)
+
+	return consultancy_list
+
+def get_occupancy_details(inpatient_record):
+	unit_list = []
 
 	service_unit_details = frappe.db.sql("""
-		SELECT io.service_unit, io.check_in, io.amount, hsu.service_unit_type, ic.rate
-		FROM `tabInpatient Occupancy` io
-		INNER JOIN `tabHealthcare Service Unit` hsu ON io.service_unit = hsu.name
-		LEFT JOIN `tabInpatient Consultancy` ic ON io.parent = ic.parent 
-				AND io.check_in = ic.date AND ic.is_confirmed = 1
-		WHERE io.is_confirmed = 1
-		AND io.parent = %s
-		ORDER BY io.check_in asc
-	"""%frappe.db.escape(parent), as_dict=1)
+        SELECT io.service_unit, Date(io.check_in) AS check_in, io.amount AS amount, 
+                hsu.service_unit_type
+        FROM `tabInpatient Occupancy` io
+        INNER JOIN `tabHealthcare Service Unit` hsu ON io.service_unit = hsu.name
+        WHERE io.is_confirmed = 1
+        AND io.parent = %s
+        ORDER BY io.check_in ASC
+    """%frappe.db.escape(inpatient_record), as_dict=1)
 
-	return service_unit_details
+	checkin_list = []
+	occupancy_list = []
+	duplicated_checkin_list = []
+	for unit in service_unit_details:
+		if unit.check_in not in checkin_list:
+			checkin_list.append(unit.check_in)
+			occupancy_list.append(unit)
+		else:
+			duplicated_checkin_list.append(unit)
+    
+	for service in occupancy_list:
+		d_amount = 0
+		if duplicated_checkin_list:
+			for d in duplicated_checkin_list:
+				frappe.msgprint(str(d))
+				if service.check_in == d.check_in:
+					d_amount += d.amount
+        
+		service.update({
+            "amount": flt(service.amount) + d_amount
+        })
+		unit_list.append(service)
+	return unit_list
 
-def get_patient_balance(args):
-	
-	customer = frappe.get_value("Patient", {"name": args.patient}, ["customer"])
-	start_date = frappe.get_value("Patient Appointment", 
-		{"name": args.appointment_no, "patient": args.patient}, ["appointment_date"]
-	)
-	discharge_date = frappe.get_value("Inpatient Record", {"name": args.inpatient_record}, ["discharge_date"])
-	
-	if discharge_date:
-		end_date = discharge_date
-	else:
-		end_date = nowdate()
+def get_patient_balance(patient, company):
+	customer = frappe.get_value("Patient", {"name": patient}, ["customer"])
 
-	customer_balances = frappe.get_all("Payment Entry", filters=[
-		["party", "=", customer], ["company", "=", args.company],
-		["posting_date", "between", [start_date, end_date]] ],
-		fields=["name", "paid_amount"]
-	)
-
-	balance = 0
-	for d in customer_balances:
-		balance += d.paid_amount
+	balance = get_balance_on(party_type="Customer", party=customer, company=company)
 
 	return balance
 
 def get_report_summary(args, summary_data):
 
-	balance = get_patient_balance(args)
+	balance = get_patient_balance(args.patient, args.company)
 
 	total_amount = 0
 	for entry in summary_data:
