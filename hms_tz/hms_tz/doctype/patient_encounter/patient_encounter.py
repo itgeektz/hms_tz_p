d2a9343b5720e43cbb74a74a73d013e4f9bbf80a
aakvatech <35020381+aakvatech@users.noreply.github.com>
2022-02-05 01:06:23 +0300

    fix: If entry.prescribe not present then 0

--- hms_tz/hms_tz/doctype/patient_encounter/patient_encounter.py
+++ hms_tz/hms_tz/doctype/patient_encounter/patient_encounter.py
@@ -52,7 +52,7 @@ def create_therapy_plan(encounter):
 			doc.append('therapy_plan_details', {
 				'therapy_type': entry.therapy_type,
 				'no_of_sessions': entry.no_of_sessions,
-				"prescribe": entry.prescribe
+				"prescribe": entry.prescribe or 0
 			})
 		doc.save(ignore_permissions=True)
 		if doc.get('name'):
