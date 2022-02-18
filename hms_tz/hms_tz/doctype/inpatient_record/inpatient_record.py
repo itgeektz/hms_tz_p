77a84869e7d924aadc76163aeaf3b0decbe11c07
taha <mot1922092@sicsr.ac.in>
2022-02-18 09:56:53 +0300

    added the dialog box for inpatient record

--- hms_tz/hms_tz/doctype/inpatient_record/inpatient_record.py
+++ hms_tz/hms_tz/doctype/inpatient_record/inpatient_record.py
@@ -373,7 +373,7 @@ def get_leave_from(doctype, txt, searchfield, start, page_len, filters):
     )
 
 def validate_schedule_discharge(inpatient_record):
-     if inpatient_record.status == "Admission Scheduled":
+    if inpatient_record.status == "Admission Scheduled":
         frappe.throw(frappe.bold(
             "Cannot schedule discharge for the patient who was not admitted,<br><br>\
             Please inform receptionist to admit a patient before discharge")
