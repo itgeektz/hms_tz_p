307200fc3bd6463e0794c4d7a65a7924d471b298
Aakvatech Limited <info@aakvatech.com>
2022-03-18 06:10:08 +0100

    Merge branch 'main' of https://github.com/aakvatech/hms_tz into main

---
+++ hms_tz/hms_tz/doctype/visiting_comission/test_visiting_comission.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Aakvatech and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestVisitingComission(unittest.TestCase):
+	pass
---
+++ hms_tz/hms_tz/report/visiting_consultant_charges_report/visiting_consultant_charges_report.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2022, Aakvatech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Visiting Consultant Charges Report"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"fieldtype": "Date",
+			"label": __("From Date"),
+			'reqd': 1
+		},
+		{
+			"fieldname": "to_date",
+			"fieldtype": "Date",
+			"label": __("To Date"),
+			'reqd': 1
+		},
+		{
+			"fieldname": "company",
+			"fieldtype": "Link",
+			"label": __("Company"),
+			"options": "Company",
+			'reqd': 1
+		},
+		{
+			"fieldname": "practitioner",
+			"fieldtype": "Link",
+			"label": __("Healthcare Practitioner"),
+			"options": "Healthcare Practitioner"
+		}
+	]
+};
+
+// ,
+		
