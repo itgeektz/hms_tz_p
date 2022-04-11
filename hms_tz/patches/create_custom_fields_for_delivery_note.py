bd9dd90cd9edce12b7a20568d8985898316b77dd
eliusmgani <eliasmgani@gmail.com>
2022-04-11 10:35:52 +0000

    fix: add practitioner name on delivery note

--- hms_tz/patches/create_custom_fields_for_delivery_note.py
+++ hms_tz/patches/create_custom_fields_for_delivery_note.py
@@ -22,7 +22,20 @@ def execute():
         'fieldtype': 'Data',
         'fetch_from': 'reference_name.appointment',
         'fetch_if_empty': 1,
-        'read_only': 0
+        'read_only': 1
+    }).insert(ignore_permissions=True)
+
+    frappe.get_doc({
+        'doctype': 'Custom Field',
+        'name': 'Delivery Note-hms_tz_practitioner',
+        'dt': 'Delivery Note',
+        'label': 'Practitioner',
+        'fieldname': 'hms_tz_practitioner',
+        'insert_after': 'hms_tz_appointment_no',
+        'fieldtype': 'Data',
+        'fetch_from': 'reference_name.practitioner',
+        'fetch_if_empty': 1,
+        'read_only': 1
     }).insert(ignore_permissions=True)
 
     frappe.db.commit()
