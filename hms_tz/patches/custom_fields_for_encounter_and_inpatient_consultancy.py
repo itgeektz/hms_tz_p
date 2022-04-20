578fdabe6c935543bcd1f92220269ce85af1120b
eliusmgani <eliasmgani@gmail.com>
2022-04-20 10:01:45 +0000

    fix: missing section break field when creating a button of add chronic diagnosis, previous it was throwing an error that section break not found

--- hms_tz/patches/custom_fields_for_encounter_and_inpatient_consultancy.py
+++ hms_tz/patches/custom_fields_for_encounter_and_inpatient_consultancy.py
@@ -23,14 +23,20 @@ def execute():
         }
     ).insert(ignore_permissions=True)
 
-    section_break_64 = frappe.get_doc('Custom Field', 'Patient Encounter-section_break_64')
-    section_break_64.insert_after = 'hms_tz_add_chronic_diagnosis'
-    section_break_64.save(ignore_permissions=True)
+    frappe.get_doc({
+            'doctype': 'Custom Field',
+            'name': 'Patient Encounter-hms_tz_section_break',
+            'dt': 'Patient Encounter',
+            'fieldname': 'hms_tz_section_break',
+            'insert_after': 'hms_tz_add_chronic_diagnosis',
+            'fieldtype': 'Section Break'
+        }
+    ).insert(ignore_permissions=True)
 
     patient_encounter_preliminary_diagnosis = frappe.get_doc('Custom Field', 
         'Patient Encounter-patient_encounter_preliminary_diagnosis'
     )
-    patient_encounter_preliminary_diagnosis.insert_after = 'section_break_64'
+    patient_encounter_preliminary_diagnosis.insert_after = 'hms_tz_section_break'
     patient_encounter_preliminary_diagnosis.save(ignore_permissions=True)
 
     frappe.get_doc({
