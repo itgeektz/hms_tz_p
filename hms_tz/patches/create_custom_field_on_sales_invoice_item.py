c24bf617a9d7d726d198eb6fa20d4dee571f8dad
eliusmgani <eliasmgani@gmail.com>
2022-04-25 13:59:44 +0000

    fix: depends on expression for custom field is_lrp_item_created

--- hms_tz/patches/create_custom_field_on_sales_invoice_item.py
+++ hms_tz/patches/create_custom_field_on_sales_invoice_item.py
@@ -4,8 +4,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 def execute():
     custom_field = {'Sales Invoice Item': [dict(fieldname='hms_tz_is_lrp_item_created',
             label='Is LRP Item Created', fieldtype='Check', insert_after='reference_dn',
-            depends_on='eval: doc.reference_dt=="Lab Prescription" || doc.reference_dt==Radiology Procedure Prescription ||\
-                doc.reference_dt=="Procedure Prescription" ',
+            depends_on='eval: doc.reference_dt=="Lab Prescription" || doc.reference_dt=="Radiology Procedure Prescription" || doc.reference_dt=="Procedure Prescription" ',
             allow_on_submit=1, read_only=1, bold=1,)]
         }
     create_custom_fields(custom_field, update=True)
