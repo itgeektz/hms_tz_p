88456061c74040f794387f2e3e0eb78985791fa7
aakvatech <info@aakvatech.com>
2023-02-27 20:44:37 +0000

    fix: Incorrect references to v14 doctypes

--- hms_tz/hms_tz/doctype/patient_appointment/patient_appointment.py
+++ hms_tz/hms_tz/doctype/patient_appointment/patient_appointment.py
@@ -18,8 +18,8 @@ from frappe.model.mapper import get_mapped_doc
 from frappe import _
 import datetime
 from frappe.core.doctype.sms_settings.sms_settings import send_sms
-from erpnext.setup.doctype.employee.employee import is_holiday
-from healthcare.healthcare.doctype.healthcare_settings.healthcare_settings import (
+from erpnext.hr.doctype.employee.employee import is_holiday
+from erpnext.healthcare.doctype.healthcare_settings.healthcare_settings import (
     get_receivable_account,
     get_income_account,
 )
--- hms_tz/hms_tz/utils.py
+++ hms_tz/hms_tz/utils.py
@@ -8,10 +8,10 @@ import frappe
 from frappe import _
 from frappe.utils.formatters import format_value
 from frappe.utils import time_diff_in_hours, rounded, getdate, nowdate
-from healthcare.healthcare.doctype.healthcare_settings.healthcare_settings import (
+from erpnext.healthcare.doctype.healthcare_settings.healthcare_settings import (
     get_income_account,
 )
-from healthcare.healthcare.doctype.fee_validity.fee_validity import create_fee_validity
+from erpnext.healthcare.doctype.fee_validity.fee_validity import create_fee_validity
 from hms_tz.hms_tz.doctype.lab_test.lab_test import create_multiple
 
 
