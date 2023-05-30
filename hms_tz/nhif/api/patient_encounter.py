6932044e70791186f688958eab971803b4c32ad5
elius mgani <78465509+av-dev2@users.noreply.github.com>
2023-05-30 21:56:58 +0300

    fix: resolve conflicts on  patient_encounter.py

--- hms_tz/nhif/api/patient_encounter.py
+++ hms_tz/nhif/api/patient_encounter.py
@@ -1036,11 +1036,7 @@ def update_inpatient_record_consultancy(doc):
         row.consultation_item = item_code
         row.rate = rate
         row.encounter = doc.name
-<<<<<<< HEAD
-
-=======
         row.healthcare_practitioner = doc.practitioner
->>>>>>> ab1c2381 (feat: show practitioner name on inpatient consultancy, Aim is to show which practitiner attended the patient)
         if discount_percent > 0:
             row.hms_tz_is_discount_applied = 1
 
