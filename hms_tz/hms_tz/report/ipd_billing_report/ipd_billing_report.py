6d87d59ce116b6a1c37ff76ea4766c74f45d2af1
elius mgani <78465509+av-dev2@users.noreply.github.com>
2023-06-02 11:37:40 +0300

    chore: exclude insurance data from the report
    (cherry picked from commit dcd1ede3ad383761741750895d161bad473e9710)

--- hms_tz/hms_tz/report/ipd_billing_report/ipd_billing_report.py
+++ hms_tz/hms_tz/report/ipd_billing_report/ipd_billing_report.py
@@ -462,8 +462,8 @@ def get_data(filters):
 			cash_lrpmt_data = get_cash_lrpmt_transaction(filters)
 			if cash_lrpmt_data: data += cash_lrpmt_data
 
-			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
-			if insurance_lrpmt_data: data += insurance_lrpmt_data
+# 			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
+# 			if insurance_lrpmt_data: data += insurance_lrpmt_data
 			
 			ipd_beds = get_ipd_occupancy_transactions(filters)
 			if ipd_beds: data += ipd_beds
@@ -509,8 +509,8 @@ def get_data(filters):
 			cash_lrpmt_data = get_cash_lrpmt_transaction(filters)
 			if cash_lrpmt_data: data += cash_lrpmt_data
 
-			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
-			if insurance_lrpmt_data: data += insurance_lrpmt_data
+# 			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
+# 			if insurance_lrpmt_data: data += insurance_lrpmt_data
 
 			data = sorted(data, key=lambda d: d['date'])
 
@@ -547,8 +547,8 @@ def get_data(filters):
 			cash_lrpmt_data = get_cash_lrpmt_transaction(filters)
 			if cash_lrpmt_data: data += cash_lrpmt_data
 
-			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
-			if insurance_lrpmt_data: data += insurance_lrpmt_data
+# 			insurance_lrpmt_data = get_insurance_lrpmt_transaction(filters)
+# 			if insurance_lrpmt_data: data += insurance_lrpmt_data
 			
 			ipd_beds = get_ipd_occupancy_transactions(filters)
 			if ipd_beds: data += ipd_beds
