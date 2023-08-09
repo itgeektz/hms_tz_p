8b91392415029e0a84551365cde550d18180b8ff
elius mgani <78465509+av-dev2@users.noreply.github.com>
2023-08-09 18:03:24 +0300

    fix: conflicts on vc_cash_rate.py

--- hms_tz/nhif/doctype/vc_cash_rate/vc_cash_rate.py
+++ hms_tz/nhif/doctype/vc_cash_rate/vc_cash_rate.py
@@ -4,11 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-<<<<<<< HEAD:hms_tz/hms_tz/doctype/visiting_rate/visiting_rate.py
-
-class VisitingRate(Document):
-    pass
-=======
 class VCCashRate(Document):
 	pass
->>>>>>> 596c03c6 (feat: enchanve visiiting commission to allow handling multiple documents):hms_tz/nhif/doctype/vc_cash_rate/vc_cash_rate.py
