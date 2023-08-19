1fa87f11ba7fa303ed09b1d9461184f29aa2392c
av-dev2 <av-dev2@aakvatech.com>
2023-08-19 09:49:56 +0300

    feat: verify service approval number before providing service
    (cherry picked from commit 47d633ff2f6109eeae7b7ed5e7249bf5e1580ed1)

--- hms_tz/nhif/api/delivery_note.js
+++ hms_tz/nhif/api/delivery_note.js
@@ -77,41 +77,33 @@ frappe.ui.form.on("Delivery Note Item", {
         }
     },
     approval_number: (frm, cdt, cdn) => {
-        // 2023-07-13
-        // stop this validation for now
-        return
         let row = locals[cdt][cdn]
         if (row.approval_number != "" && row.approval_number != undefined) {
             frappe.call({
                 method: "hms_tz.nhif.api.healthcare_utils.varify_service_approval_number_for_LRPM",
                 args: {
-                    patient: frm.doc.patient,
                     company: frm.doc.company,
                     approval_number: row.approval_number,
-                    template: "Medication",
-                    item: row.item_code,
+                    template_doctype: "Medication",
+                    template_name: row.item_code,
+                    encounter: frm.doc.reference_name
                 },
                 freeze: true,
                 freeze_message: __("Verifying Approval Number..."),
             }).then(r => {
                 if (r.message) {
-                    let data = r.message;
-                    row.approval_type = "NHIF"
-                    row.approval_status = "Verified"
-                    row.authorized_item_id = data.AuthorizedItemID;
-                    row.service_authorization_id = data.ServiceAuthorizationID;
                     frappe.show_alert({
                         message: __("<h4 class='text-center' style='background-color: #D3D3D3; font-weight: bold;'>\
                             Approval Number is Valid</h4>"),
                         indicator: "green"
-                    }, 10);
+                    }, 20);
                 } else {
                     row.approval_number = ""
                     frappe.show_alert({
                         message: __("<h4 class='text-center' style='background-color: #D3D3D3; font-weight: bold;'>\
                             Approval Number is not Valid</h4>"),
                         indicator: "Red"
-                    }, 10);
+                    }, 20);
                 }
             });
         }
